cce19e57e40b281860ff0c410359f669a8fe809f
Binh Nguyen <b.nguyen.sn@gmail.com>
2023-01-15 13:42:53 +0000

    feat: add Input component and tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 describe('App', () => {
   it('renders the app', () => {
     render(<App />);
-    expect(screen.getByText('Cassidoo Template')).toBeDefined();
+    expect(screen.getByText('Cassidoo Template')).toBeVisible();
   });
 });
